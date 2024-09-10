2da2752e599c239300abc7c0db01dc6b958beea9
jaimesHub <duongquockhanh230596@gmail.com>
2024-09-10 13:51:54 +0700

    printf, println functions

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -2,8 +2,16 @@ package javacore;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+//		System.out.printf("Hello World");
+		System.out.printf("Hello World %dnth", 1000);
+		System.out.printf("Hello World %.3f nth", 40.1235312);
+
+		String name = "James";
 		int age = 28;
-		int test = 5 + 9 * 3 - 10 / 2;
-		System.out.println(test);
+
+		System.out.printf("\nMy name is %s, age = %d\n", name, age);
+		System.out.println("My name is " + name + ", age = " + age);
 	}
 }
