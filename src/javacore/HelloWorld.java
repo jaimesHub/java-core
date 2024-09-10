f634dd626d276db2ebd98e105c2d845ef93fd9dc
jaimesHub <duongquockhanh230596@gmail.com>
2024-09-10 13:56:56 +0700

    Scanner - input from keyboard

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -1,17 +1,15 @@
 package javacore;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-//		System.out.printf("Hello World");
-		System.out.printf("Hello World %dnth", 1000);
-		System.out.printf("Hello World %.3f nth", 40.1235312);
-
-		String name = "James";
-		int age = 28;
-
-		System.out.printf("\nMy name is %s, age = %d\n", name, age);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
 		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
